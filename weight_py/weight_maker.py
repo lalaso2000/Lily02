5b2d1f18a68410afffa36c2d013ded5fedc8098c
lalaso2000 <raraso2000@icloud.com>
2020-01-08 21:26:11 +0900

    乱数生成スクリプトを修正

--- weight_py/weight_maker.py
+++ weight_py/weight_maker.py
@@ -1,4 +1,10 @@
 import numpy as np
 
-r = np.random.rand(12, 54) * 2.0 - 1.0
-np.savetxt('weight_rand1.csv', r, fmt="%.4f", delimiter=',')
+r1 = np.random.rand(60, 16) * 2.0 - 1.0
+r2 = np.random.rand(16, 10) * 2.0 - 1.0
+r3 = np.random.rand(10, 32) * 2.0 - 1.0
+
+with open('weight_rand.csv', 'w') as f:
+  np.savetxt(f, r1, fmt="%.4f", delimiter=',')
+  np.savetxt(f, r2, fmt="%.4f", delimiter=',')
+  np.savetxt(f, r3, fmt="%.4f", delimiter=',')
